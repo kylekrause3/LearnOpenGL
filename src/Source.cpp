4f1a8fdb3298717c410632bdfb7c252d3a73b2cd
kylekrause3 <kkrause317@gmail.com>
2023-03-01 13:52:26 -0500

    changed controls space -> up arrow: camera moves up shift -> down arrow: camera moves down

--- src/Source.cpp
+++ src/Source.cpp
@@ -304,10 +304,10 @@ void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
         camera.ProcessKeyboard(RIGHT, deltaTime);
     }
-    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
         camera.ProcessKeyboard(UP, deltaTime);
     }
-    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS) {
         camera.ProcessKeyboard(DOWN, deltaTime);
     }
 }
